ba701832db04df63dbc6e6fb4aecd9884ba07d0e
matiascfgm <matiascfgm@gmail.com>
2019-10-21 23:58:38 +0200

    Added a comment on the interface

--- src/app/interfaces/task.ts
+++ src/app/interfaces/task.ts
@@ -2,6 +2,9 @@ import { TaskState } from '../enums/task-state.enum';
 
 
 export interface Task {
+    /**
+     * @var id It is is a string because Firebase uses HASHes instead of IDs
+     */
     id: string;
 
     name: string;
